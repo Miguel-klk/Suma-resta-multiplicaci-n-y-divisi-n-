9b3d3b653c943280ce703642a52aaf71b8e34923
Miguel-klk <124157850+Miguel-klk@users.noreply.github.com>
2023-04-13 11:07:40 +0200

    ge

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let num2 = parseFloat(prompt("Ahora, introduzca el segundo número: "));
 let suma = num1 + num2;
 let resta = num1 - num2;
 let multiplicacion = num1 * num2;
-let division = num2 !== 0 ? num1 / num2 : "ERROR.";
+let division = num2 !== 0 ? num1 / num2 : "ERROR";
 
 alert("El resultado de la suma es: " + suma);
 alert("El resultado de la resta es: " + resta);
