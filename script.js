1a828383e572f94fab9aa7a8362f35e5e9ff3653
Miguel-klk <124157850+Miguel-klk@users.noreply.github.com>
2023-04-13 11:02:09 +0200

    suma

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Desde aquí, con 2 números, realizaremos diversas operaciones")
+let num1 = parseFloat(prompt("Por favor, ingrese el primer número: "));
+
+let num2 = parseFloat(prompt("Ahora, ingrese el segundo número: "));
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num2 !== 0 ? num1 / num2 : "No se puede dividir por cero.";
+
+alert("El resultado de la suma es: " + suma);
+alert("El resultado de la resta es: " + resta);
+alert("El resultado de la multiplicación es: " + multiplicacion);
+alert("El resultado de la división es: " + division);
